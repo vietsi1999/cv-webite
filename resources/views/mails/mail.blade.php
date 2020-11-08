a864c0f000425169bf9244ba711f6a1d5df211ca
vietsi1999 <vietsi1999@gmail.com>
2020-11-08 17:50:50 +0700

    Download CV

---
+++ resources/views/mails/mail.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CV - Website</title>
+</head>
+<body>
+	<p>
+		{{__('mail_content, :name',['name' => $email])}}
+	</p>
+	<a href="http://127.0.0.1:8000/downloadCV">Curriculum_Vitae_Tran_Viet_Si.pdf</a>
+</body>
+</html> 
